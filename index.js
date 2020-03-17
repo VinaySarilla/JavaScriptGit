5cacb2ecf72aabe663502d87e115a5cacc07a00c
VinaySarilla <vinayknown@gmail.com>
2020-03-17 17:57:11 +0530

    sub function

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var button = document.querySelector("button");
 var inum1 = document.getElementById("num1");
 var inum2 = document.getElementById("num2");
 function add(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
     return num1 - num2;
 }
 button.addEventListener("click", function () {
